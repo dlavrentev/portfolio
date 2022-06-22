94937b0d03a4ab34bf3555729d1a1958c5006cac
dlavrentev <dimitrilavrentev96@gmail.com>
2022-06-22 16:53:28 +0200

    Added Image and Work on About.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './Style.css';
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
+import About from './Components/Body/About';
 
 function App() {
   return (
--- src/Components/Body/About.js
+++ src/Components/Body/About.js
@@ -1,8 +1,23 @@
 import React from 'react'
 import '../../Style.css'
+import Image from '../../Img/profilepic.PNG'
 
 export default function About() {
   return (
-    <div className='About-Container'>About</div>
+    <div className='about-container'>
+        <div className='about-body-left'>
+            <h1>Hallo! I'm a</h1>
+            <h1>frontend developer</h1>
+            <h1>in making.</h1>
+            <p>I'm Dimitri! A german-based programming enthusiast with the goal to get his first 
+                job as a frontend developer!
+            </p>
+            <button>Look at my work!</button>
+
+        </div>
+        <div className='about-body-right'> 
+                <img src={Image} alt='profile' className='profile-image'></img>
+        </div>
+    </div>
   )
 }
