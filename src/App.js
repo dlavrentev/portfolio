d486400a06b5e5b2f6a6e2bdea90b47129095533
dlavrentev <dimitrilavrentev96@gmail.com>
2022-06-22 16:27:39 +0200

    Created Header, Body and Footer Components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './Style.css';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <About />
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Body/About.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import '../../Style.css'
+
+export default function About() {
+  return (
+    <div className='About-Container'>About</div>
+  )
+}
