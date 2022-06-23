4536e68653e58086612ac952819399db85db9031
dlavrentev <dimitrilavrentev96@gmail.com>
2022-06-23 14:21:43 +0200

    started with header component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import './Header.css'
+import Logo from '../../Img/dllogo.png'
 
 export default function Header() {
   return (
     <div className='header-container'>
-        
+        <img className='logo-header' alt='logo' src={Logo}></img>
+        <div className='nav-container'>
+            <div className='nav-a'>About</div>
+            <div className='nav-a'>Work</div>
+            <div className='nav-a'>Services</div>
+            <div className='nav-a'>Contact</div>
+        </div>
+        <div className='circle-container'>
+            <div className='circle'>1</div>
+            <div className='circle'>2</div>
+            <div className='circle'>3</div>
+        </div>
     </div>
   )
 }
