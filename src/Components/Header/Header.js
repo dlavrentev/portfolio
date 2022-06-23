a08c046a4c4522ac32fddefdb07d49f70482d706
dlavrentev <dimitrilavrentev96@gmail.com>
2022-06-23 15:06:39 +0200

    updated header component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,18 +5,39 @@ import Logo from '../../Img/dllogo.png'
 export default function Header() {
   return (
     <div className='header-container'>
+        {/* logo  */}
         <img className='logo-header' alt='logo' src={Logo}></img>
+        {/* navigation */}
         <div className='nav-container'>
             <div className='nav-a'>About</div>
             <div className='nav-a'>Work</div>
             <div className='nav-a'>Services</div>
             <div className='nav-a'>Contact</div>
         </div>
-        <div className='circle-container'>
-            <div className='circle'></div>
-            <div className='circle'></div>
-            <div className='circle'></div>
+        {/* social media buttons  */}
+        <div class="wrapper">
+          <button class="btn">
+            <i class="ri-share-line"></i>
+          </button>
+          <ul class="list">
+            <li class="item">
+              <a href="#" class="link ig">
+                <i class="ri-instagram-line"></i>
+              </a>
+            </li>
+            <li class="item" style="--d: .25s;">
+              <a href="#" class="link tw">
+                <i class="ri-twitter-line"></i>
+              </a>
+            </li>
+            <li class="item" style="--d: .5s;">
+              <a href="#" class="link sc">
+                <i class="ri-snapchat-line"></i>
+              </a>
+            </li>
+          </ul>
         </div>
+    
     </div>
   )
 }
