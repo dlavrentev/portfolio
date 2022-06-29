a2c163b702f8d4b410aa5995083692a6103c4d7c
dlavrentev <dimitrilavrentev96@gmail.com>
2022-06-29 20:04:46 +0200

    updated about.js

--- src/Components/Body/About.js
+++ src/Components/Body/About.js
@@ -7,12 +7,10 @@ export default function About() {
   return (
     <div className='about-container'>
         <div className='about-body-left'>
-            <h1>Hallo! I'm a</h1>
-            <h1><span className='h1-custom'>frontend</span>-developer</h1>
-            <h1>in making.</h1>
-            <p>I'm Dimitri! A german-based programming enthusiast with the goal 
-            </p>
-            <p>to get his first 
+            <h1>Hallo! I'm a
+            <span className='h1-custom'> frontend</span> developer
+            in making.</h1>
+            <p>I'm Dimitri! A german-based programming enthusiast with the goal to get his first 
                 job as a frontend developer!</p>
             <button>Look at my work!</button>
 
